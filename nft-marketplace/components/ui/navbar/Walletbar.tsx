30dc2acea74e175aa0ea5db184537e5bd31f9268
= <kang5647@LAPTOP-AP9R46KT.localdomain>
2023-02-03 15:55:53 +0800

    updated wallet and network display

--- nft-marketplace/components/ui/navbar/Walletbar.tsx
+++ nft-marketplace/components/ui/navbar/Walletbar.tsx
@@ -106,7 +106,7 @@ const Walletbar: FunctionComponent<WalletbarProps> = () => {
       <div>
         <button
           onClick={() => {
-            window.open("https://metamask.io", "_ blank");
+            window.open("https://polkadot.js.org/", "_ blank");
           }}
           type="button"
           className="inline-flex items-center px-3 py-1.5 border border-transparent text-xs font-medium rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
--- nft-marketplace/components/ui/navbar/index.tsx
+++ nft-marketplace/components/ui/navbar/index.tsx
@@ -16,7 +16,8 @@ function classNames(...classes: string[]) {
 }
 
 export default function Navbar() {
-  const { accounts, connect, isConnecting, isConnected } = useInkathon();
+  const { accounts, connect, isConnecting, isConnected, activeChain } =
+    useInkathon();
 
   return (
     <Disclosure as="nav" className="bg-gray-800">
@@ -57,6 +58,22 @@ export default function Navbar() {
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                <div className="text-gray-300 self-center mr-2">
+                  <span className="inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium bg-purple-100 text-purple-800">
+                    <svg
+                      className="-ml-0.5 mr-1.5 h-2 w-2 text-indigo-400"
+                      fill="currentColor"
+                      viewBox="0 0 8 8"
+                    >
+                      <circle cx={4} cy={4} r={3} />
+                    </svg>
+                    {isConnecting
+                      ? "Loading..."
+                      : isConnected
+                      ? activeChain?.network
+                      : "Install Web3 Wallet"}
+                  </span>
+                </div>
                 <Walletbar />
               </div>
             </div>
