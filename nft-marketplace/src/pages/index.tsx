7636961a8cc45ea477bbbc0b4fe2e8a0fdc30b6a
Derrick Png <derrickpng34@gmail.com>
2023-02-15 02:38:53 +0800

    Fixed Homepage Retrieval Bug

--- nft-marketplace/src/pages/index.tsx
+++ nft-marketplace/src/pages/index.tsx
@@ -73,7 +73,7 @@ const Home: NextPage = () => {
   };
   useEffect(() => {
     getAllNFTs();
-  }, []);
+  }, [api, contract, account]);
   return (
     <BaseLayout>
       <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
